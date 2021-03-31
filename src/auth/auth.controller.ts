1fc3af53d315108be975143ca18e310e2cd9f5e2
Roman <romanturaj@gmail.com>
2021-03-31 20:36:21 +0200

    authorization part 1

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post,  ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) { }
+  
+  @Post('/sign-up')
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto):Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/sign-in')
+  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto):Promise<{accessToken: string}> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+
+}
---
+++ src/auth/auth.repository.ts
@@ -0,0 +1,40 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { EntityRepository, Repository } from "typeorm"
+import { AuthCredentialsDto } from "./auth-credentials.dto"
+import { User } from "./user.entity"
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UserRepository extends Repository <User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { email, password } = authCredentialsDto;
+    
+    const user = new User();
+    user.email = email;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password,user.salt);
+    user.active = false;
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+      throw new ConflictException('email already exists')
+      } else {
+        throw new InternalServerErrorException()
+      }
+    }
+    
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string>{
+    const { email, password } = authCredentialsDto;
+    const user = await this.findOne({ email });
+    if (user && await user.validatePassword(password)) {
+      return user.email;
+    }
+    return null;
+  }
+  private async hashPassword(password: string, salt: string): Promise<string>{
+    return bcrypt.hash(password, salt);
+  }
+}
