e3f6b914033a810a6074ba039621021bc59f1653
Julita Turaj <pogojulita@gmail.com>
2021-04-18 18:34:26 +0200

    upload cv

--- src/modules/candidates/candidates.controller.ts
+++ src/modules/candidates/candidates.controller.ts
@@ -1,11 +1,14 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
   HttpCode,
   Post,
   Query,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
   ValidationPipe,
 } from '@nestjs/common';
 import { CandidateDto } from 'src/modules/candidates/dto/candidate.dto';
@@ -15,12 +18,16 @@ import { CandidateFilterDto } from './dto/candidate-filter.dto';
 import { Candidate } from './entities/candidate.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../auth/user.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { File } from '../../common/interfaces/file.interface';
+import { S3ManagerService } from '../s3manager/s3-manager.service';
 
 
 @Controller('candidates')
 export class CandidatesController {
   constructor(
     private candidatesService: CandidatesService,
+    private s3ManagerService: S3ManagerService,
   ) { }
 
   @UseGuards(AuthGuard())
@@ -34,6 +41,29 @@ export class CandidatesController {
   @HttpCode(201)
   create(@Body(ValidationPipe) data: CandidateDto,): Promise<Candidate> {
     return this.candidatesService.create(data);
+  }
+
+
+  @Post('upload-cv')
+  @HttpCode(201)
+  @UseInterceptors(FileInterceptor('file'))
+  async createCV(
+    @UploadedFile() file: File,
+  ) {
+
+    if (file.mimetype !== "application/pdf") {
+      throw new BadRequestException("File must have .pdf extension")
+    }
+    const upload = await this.s3ManagerService
+      .uploadFile('cvs', file)
+      .catch(() => {
+        throw new BadRequestException(
+          'There was problem during uploading file',
+        );
+      });
+
+
+    return { file_path: upload.Location };
   }
 
 }
