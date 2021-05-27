786e15c247cb8f527742c7062c527aa2d22dc7c2
kheigenbarth <kamil.heigenbarth@fibr.com>
2021-05-27 12:59:34 +0200

    start prod

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/app/backend || exit
 mkdir postgres-data
 sudo cp ../envs/backend.env .env
 git checkout dev
-/usr/local/bin/docker-compose up -d
+/usr/local/bin/docker-compose up -d prod
