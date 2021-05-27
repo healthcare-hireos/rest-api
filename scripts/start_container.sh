79734c04b6538ada98035fc56c2c433a11ff4e7d
kheigenbarth <kamil.heigenbarth@fibr.com>
2021-05-27 12:39:32 +0200

    credentials should work, fix dep group, run migrations

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,4 +2,6 @@
 cd /home/ec2-user/app/backend || exit
 sudo cp ../envs/backend.env .env
 git checkout dev
-/usr/local/bin/docker-compose up -d dev
+/usr/local/bin/docker-compose up -d
+npm run migration:run
+npm run seed:run
