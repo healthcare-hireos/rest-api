5aeba19d8597c8425332ebe2a87ed5e88532bc79
kheigenbarth <kamil.heigenbarth@fibr.com>
2021-05-27 15:50:03 +0200

    prod working

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/app/backend || exit
 sudo chmod -R 755 postgres-data
 sudo cp ../envs/backend.env .env
-/usr/local/bin/docker-compose up --build -V --force-recreate -d prod
+/usr/local/bin/docker-compose up --build -V -d prod
