decd84f017024cf8ccbd285225ffa01c53bebea4
kheigenbarth <kamil.heigenbarth@fibr.com>
2021-05-27 12:47:36 +0200

    chmod on pg data/*

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /home/ec2-user/app/backend || exit
 sudo cp ../envs/backend.env .env
-sudo chmod 744 postgres-data
+sudo chmod 744 postgres-data/*
 git checkout dev
 /usr/local/bin/docker-compose up -d
